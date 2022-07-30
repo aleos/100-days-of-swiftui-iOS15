a8a87b253037a89cbf882f6441414e19695da156
Alexander Ostrovsky <thealeos@gmail.com>
2022-07-30 23:26:28 +0300

    Create project in Xcode

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Alexander Ostrovsky on 30.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
