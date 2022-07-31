47be8e1a349a41fea7a0094ab7b5c812feb0b4eb
Alexander Ostrovsky <thealeos@gmail.com>
2022-07-31 13:58:22 +0300

    Add unit conversion interface and logic

--- UnitConverter/UnitConverter/ContentView.swift
+++ UnitConverter/UnitConverter/ContentView.swift
@@ -8,9 +8,47 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var value = 0.0
+    @State private var fromSelection: UnitVolume = .liters
+    @State private var toSelection: UnitVolume = .gallons
+    
+    let units: [UnitVolume] = [.milliliters, .liters, .cups, .pints, .gallons]
+    var converted: Measurement<UnitVolume> {
+        Measurement(value: value, unit: fromSelection).converted(to: toSelection)
+    }
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            Form {
+                Section {
+                    TextField("Enter the value", value: $value, format: .number)
+                } header: {
+                    Text("Value")
+                }
+                
+                Section {
+                    Picker("From", selection: $fromSelection) {
+                        ForEach(units, id: \.symbol) {
+                            Text("\($0.symbol)").tag($0)
+                        }
+                    }
+                    Picker("To", selection: $toSelection) {
+                        ForEach(units, id: \.symbol) {
+                            Text("\($0.symbol)").tag($0)
+                        }
+                    }
+                } header: {
+                    Text("Units")
+                }
+                
+                Section {
+                    Text("\(converted.formatted(.measurement(width: .abbreviated, usage: .asProvided)))")
+                } header: {
+                    Text("Result")
+                }
+            }
+            .navigationTitle("UnitConverter")
+        }
     }
 }
 
