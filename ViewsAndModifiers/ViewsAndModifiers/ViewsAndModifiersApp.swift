fc5e909232d059401b05c0fd74c481a6e46a63c3
Alexander Ostrovsky <thealeos@gmail.com>
2022-07-31 19:50:43 +0300

    Create a new app: ViewsAndModifiers

---
+++ ViewsAndModifiers/ViewsAndModifiers/ViewsAndModifiersApp.swift
@@ -0,0 +1,17 @@
+//
+//  ViewsAndModifiersApp.swift
+//  ViewsAndModifiers
+//
+//  Created by Alexander Ostrovsky on 31.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct ViewsAndModifiersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
