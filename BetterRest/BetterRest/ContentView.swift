b304f5391398bb60ba085889c27350c5081583c1
Alexander Ostrovsky <thealeos@gmail.com>
2022-08-03 22:08:03 +0300

    Replace the “Number of cups” stepper with a Picker showing the same range of values.

--- BetterRest/BetterRest/ContentView.swift
+++ BetterRest/BetterRest/ContentView.swift
@@ -40,7 +40,11 @@ struct ContentView: View {
                 }
                 
                 Section {
-                    Stepper(coffeeAmount == 1 ? "1 cup" : "\(coffeeAmount) cups", value: $coffeeAmount, in: 1...20)
+                    Picker("Number of cups per day", selection: $coffeeAmount) {
+                        ForEach(1...20, id: \.self) { cups in
+                            Text(cups == 1 ? "1 cup" : "\(cups) cups")
+                        }
+                    }
                 } header: {
                     Text("Daily coffee intake")
                 }
