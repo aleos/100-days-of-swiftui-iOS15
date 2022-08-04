f68f2f1517b4737611a7edb559091f350d13678e
Alexander Ostrovsky <thealeos@gmail.com>
2022-08-04 22:35:53 +0300

    Add project WordScramble (Project 5)

--- README.md
+++ README.md
@@ -1,9 +1,11 @@
 # 100 Days of SwiftUI
 
-Projects:
+## Projects:
 - WeSplit (Project 1)
 - UnitConverter (Challenge)
 - GuessTheFlag (Project 2)
 - ViewsAndModifiers (Project 3)
 - RockPaperScissors (Challenge)
--
+- BetterRest (Project 4)
+- WordScramble (Project 5)
+- 
---
+++ WordScramble/WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Alexander Ostrovsky on 04.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
