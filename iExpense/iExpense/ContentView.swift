e9dc8e290843cd739ce53e6e712e913481cb521c
Alexander Ostrovsky <thealeos@gmail.com>
2022-08-09 23:48:15 +0300

    Add `dismiss` example

--- iExpense/iExpense/ContentView.swift
+++ iExpense/iExpense/ContentView.swift
@@ -7,20 +7,26 @@
 
 import SwiftUI
 
-class User: ObservableObject {
-    @Published var firstName = "Bilbo"
-    @Published var lastName = "Baggins"
+struct ContentView: View {
+    @State private var showingSheet = false
+    
+    var body: some View {
+        Button("Show Sheet") {
+            showingSheet.toggle()
+        }
+        .sheet(isPresented: $showingSheet) {
+            SecondView(name: "aleos")
+        }
+    }
 }
 
-struct ContentView: View {
-    @StateObject var user = User()
+struct SecondView: View {
+    @Environment(\.dismiss) var dismiss
+    let name: String
 
     var body: some View {
-        VStack {
-            Text("Your name is \(user.firstName) \(user.lastName).")
-
-            TextField("First name", text: $user.firstName)
-            TextField("Last name", text: $user.lastName)
+        Button("Dismiss") {
+            dismiss()
         }
     }
 }
