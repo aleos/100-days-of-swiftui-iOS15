fa5001b8e68241a675bbcf8888b41365911c64d1
Alexander Ostrovsky <thealeos@gmail.com>
2022-08-13 13:40:04 +0300

    Extract `GridItemView`

--- Moonshot/Moonshot/ContentView.swift
+++ Moonshot/Moonshot/ContentView.swift
@@ -25,30 +25,7 @@ struct ContentView: View {
                         NavigationLink {
                             MissionView(mission: mission, astronauts: astronauts)
                         } label: {
-                            VStack {
-                                Image(mission.image)
-                                    .resizable()
-                                    .scaledToFit()
-                                    .frame(width: 100, height: 100)
-                                    .padding()
-
-                                VStack {
-                                    Text(mission.displayName)
-                                        .font(.headline)
-                                        .foregroundColor(.white)
-                                    Text(mission.formattedLaunchDate)
-                                        .font(.caption)
-                                        .foregroundColor(.white.opacity(0.5))
-                                }
-                                .padding(.vertical)
-                                .frame(maxWidth: .infinity)
-                                .background(.lightBackground)
-                            }
-                            .clipShape(RoundedRectangle(cornerRadius: 10))
-                            .overlay(
-                                RoundedRectangle(cornerRadius: 10)
-                                    .stroke(.lightBackground)
-                            )
+                            GridItemView(mission: mission)
                         }
                     }
                 }
@@ -73,5 +50,35 @@ struct ContentView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct GridItemView: View {
+    let mission: Mission
+    var body: some View {
+        VStack {
+            Image(mission.image)
+                .resizable()
+                .scaledToFit()
+                .frame(width: 100, height: 100)
+                .padding()
+            
+            VStack {
+                Text(mission.displayName)
+                    .font(.headline)
+                    .foregroundColor(.white)
+                Text(mission.formattedLaunchDate)
+                    .font(.caption)
+                    .foregroundColor(.white.opacity(0.5))
+            }
+            .padding(.vertical)
+            .frame(maxWidth: .infinity)
+            .background(.lightBackground)
+        }
+        .clipShape(RoundedRectangle(cornerRadius: 10))
+        .overlay(
+            RoundedRectangle(cornerRadius: 10)
+                .stroke(.lightBackground)
+        )
     }
 }
