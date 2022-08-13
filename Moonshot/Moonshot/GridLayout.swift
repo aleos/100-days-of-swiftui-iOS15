bf8f6156c659e0741d051db0baefa20d045f8fe8
Alexander Ostrovsky <thealeos@gmail.com>
2022-08-13 13:47:00 +0300

    Extract `GridLayout`

---
+++ Moonshot/Moonshot/GridLayout.swift
@@ -0,0 +1,73 @@
+//
+//  GridLayout.swift
+//  Moonshot
+//
+//  Created by Alexander Ostrovsky on 13.08.2022.
+//
+
+import SwiftUI
+
+struct GridLayout: View {
+    let astronauts: [String: Astronaut]
+    let missions: [Mission]
+    
+    let columns = [
+        GridItem(.adaptive(minimum: 150))
+    ]
+    
+    var body: some View {
+        LazyVGrid(columns: columns) {
+            ForEach(missions) { mission in
+                NavigationLink {
+                    MissionView(mission: mission, astronauts: astronauts)
+                } label: {
+                    GridItemView(mission: mission)
+                }
+            }
+        }
+        .padding([.horizontal, .bottom])
+    }
+}
+
+struct GridItemView: View {
+    let mission: Mission
+    var body: some View {
+        VStack {
+            Image(mission.image)
+                .resizable()
+                .scaledToFit()
+                .frame(width: 100, height: 100)
+                .padding()
+            
+            VStack {
+                Text(mission.displayName)
+                    .font(.headline)
+                    .foregroundColor(.white)
+                Text(mission.formattedLaunchDate)
+                    .font(.caption)
+                    .foregroundColor(.white.opacity(0.5))
+            }
+            .padding(.vertical)
+            .frame(maxWidth: .infinity)
+            .background(.lightBackground)
+        }
+        .clipShape(RoundedRectangle(cornerRadius: 10))
+        .overlay(
+            RoundedRectangle(cornerRadius: 10)
+                .stroke(.lightBackground)
+        )
+    }
+}
+
+struct GridLayout_Previews: PreviewProvider {
+    static let missions: [Mission] = Bundle.main.decode("missions.json")
+    static let astronauts: [String: Astronaut] = Bundle.main.decode("astronauts.json")
+
+    static var previews: some View {
+        ScrollView {
+            GridLayout(astronauts: astronauts, missions: missions)
+        }
+        .background(.darkBackground)
+        .preferredColorScheme(.dark)
+    }
+}
