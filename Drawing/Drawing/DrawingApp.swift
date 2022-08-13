4f205c3b838b3e4d32c6cfeab2f7028e49613db3
Alexander Ostrovsky <thealeos@gmail.com>
2022-08-13 14:41:08 +0300

    Create the new project: `Drawing`

---
+++ Drawing/Drawing/DrawingApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrawingApp.swift
+//  Drawing
+//
+//  Created by Alexander Ostrovsky on 13.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct DrawingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
