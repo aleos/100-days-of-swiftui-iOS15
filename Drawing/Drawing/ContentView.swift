f184d1ca4b99832e15d997abc3ea9d3f5b840d02
Alexander Ostrovsky <thealeos@gmail.com>
2022-08-13 18:00:14 +0300

    Add example of using an `ImagePaint`

--- Drawing/Drawing/ContentView.swift
+++ Drawing/Drawing/ContentView.swift
@@ -23,6 +23,8 @@ struct ContentView: View {
                         .navigationTitle("⭕️ Arc")
                 }
                 NavigationLink("🌼 Flower") { FlowerContentView() }
+                NavigationLink("🖼 Frame") { ImagePaintContentView() }
+                NavigationLink("Triangle") { Triangle() }
                 NavigationLink("Triangle") { Triangle() }
                 NavigationLink("Triangle") { Triangle() }
                 NavigationLink("Triangle") { Triangle() }
@@ -125,6 +127,20 @@ struct FlowerContentView: View {
         .tint(.orange)
         .padding()
         .navigationTitle("🌼 Flower")
+    }
+}
+
+struct ImagePaintContentView: View {
+    var body: some View {
+        VStack {
+            Text("Hello World")
+                .frame(width: 300, height: 300)
+                .border(ImagePaint(image: Image("Example"), sourceRect: CGRect(x: 0, y: 0.4, width: 1, height: 0.2), scale: 0.1), width: 50)
+            Capsule()
+                .strokeBorder(ImagePaint(image: Image("Example"), sourceRect: CGRect(x: 0, y: 0.25, width: 1, height: 0.5), scale: 0.3), lineWidth: 20)
+                .frame(width: 300, height: 200)
+        }
+        .navigationTitle("🖼 Frame")
     }
 }
 
