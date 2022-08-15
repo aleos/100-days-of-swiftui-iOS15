72389d97d5d855fcb102ee36d0a093dc2cfaf5e2
Alexander Ostrovsky <thealeos@gmail.com>
2022-08-15 23:20:46 +0300

    Add `HabitTracker` app

---
+++ HabitTracker/HabitTracker/HabitTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitTrackerApp.swift
+//  HabitTracker
+//
+//  Created by Alexander Ostrovsky on 15.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct HabitTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
