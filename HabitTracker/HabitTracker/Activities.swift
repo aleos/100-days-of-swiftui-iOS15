3b033b6dca68ff07eb2495c2fc51482dc2312425
Alexander Ostrovsky <thealeos@gmail.com>
2022-08-16 23:22:49 +0300

    Add base activity data structs

---
+++ HabitTracker/HabitTracker/Activities.swift
@@ -0,0 +1,12 @@
+//
+//  Activities.swift
+//  HabitTracker
+//
+//  Created by Alexander Ostrovsky on 16.08.2022.
+//
+
+import Foundation
+
+class Activities: ObservableObject {
+    @Published activities: [Activity] = []
+}
---
+++ HabitTracker/HabitTracker/Activity.swift
@@ -0,0 +1,12 @@
+//
+//  Activity.swift
+//  HabitTracker
+//
+//  Created by Alexander Ostrovsky on 16.08.2022.
+//
+
+import Foundation
+
+struct Activity: Codable, Identifiable {
+    var id = UUID()
+}
