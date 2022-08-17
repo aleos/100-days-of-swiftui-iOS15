fbf4cbf514b693b6188fb491582ffbce8ea25569
Alexander Ostrovsky <thealeos@gmail.com>
2022-08-17 21:58:56 +0300

    Add save/load for activities

--- HabitTracker/HabitTracker/Activities.swift
+++ HabitTracker/HabitTracker/Activities.swift
@@ -8,7 +8,17 @@
 import Foundation
 
 class Activities: ObservableObject {
-    @Published var activities: [Activity] = []
+    @Published var activities: [Activity] = [] {
+        didSet {
+            guard let data = try? JSONEncoder().encode(activities) else { return }
+            UserDefaults.standard.set(data, forKey: "activities")
+        }
+    }
+    
+    init() {
+        guard let activities = UserDefaults.standard.data(forKey: "activities").flatMap({ try? JSONDecoder().decode([Activity].self, from: $0) }) else { return }
+        self.activities = activities
+    }
     
     // MARK: Intents
     func add(_ name: String, description: String) {
