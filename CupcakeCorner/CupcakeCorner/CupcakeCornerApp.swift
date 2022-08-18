aaba9d86f828cc51d28607fe678a5e68b09a6d53
Alexander Ostrovsky <thealeos@gmail.com>
2022-08-18 20:55:49 +0300

    Add the `CupcakeCorner` project

---
+++ CupcakeCorner/CupcakeCorner/CupcakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeCornerApp.swift
+//  CupcakeCorner
+//
+//  Created by Alexander Ostrovsky on 18.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
