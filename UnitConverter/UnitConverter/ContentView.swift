0b949b6d76e9318667f6589e6aed135bf598a3f6
Alexander Ostrovsky <thealeos@gmail.com>
2022-08-21 14:24:46 +0300

    Add support of different dimensions

--- UnitConverter/UnitConverter/ContentView.swift
+++ UnitConverter/UnitConverter/ContentView.swift
@@ -8,35 +8,59 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var value = 0.0
-    @State private var fromSelection: UnitVolume = .liters
-    @State private var toSelection: UnitVolume = .gallons
-    @FocusState private var valueIsFocused: Bool
+    @State private var input = 1.0
+    @State private var dimension: Conversion = .volume
+    @State private var inputUnit: Dimension = UnitVolume.liters
+    @State private var outputUnit: Dimension = UnitVolume.gallons
+    @FocusState private var inputIsFocused: Bool
     
-    let units: [UnitVolume] = [.milliliters, .liters, .cups, .pints, .gallons]
-    var converted: Measurement<UnitVolume> {
-        Measurement(value: value, unit: fromSelection).converted(to: toSelection)
+    enum Conversion: String, CaseIterable {
+        case duration, length, mass, temperature, volume
+        
+        var units: [Dimension] {
+            switch self {
+            case .duration:
+                return [UnitDuration]([.hours, .minutes, .seconds])
+            case .length:
+                return [UnitLength]([.meters, .kilometers, .feet, .yards, .miles])
+            case .mass:
+                return [UnitMass]([.grams, .kilograms, .ounces, .pounds])
+            case .temperature:
+                return [UnitTemperature]([.celsius, .fahrenheit, .kelvin])
+            case .volume:
+                return [UnitVolume]([.milliliters, .liters, .cups, .pints, .gallons])
+            }
+        }
+    }
+    
+    var converted: Measurement<Dimension> {
+        Measurement(value: input, unit: inputUnit).converted(to: outputUnit)
     }
     
     var body: some View {
         NavigationView {
             Form {
                 Section {
-                    TextField("Enter the value", value: $value, format: .number)
+                    TextField("Enter the value", value: $input, format: .number)
                         .keyboardType(.decimalPad)
-                        .focused($valueIsFocused)
+                        .focused($inputIsFocused)
                 } header: {
                     Text("Value")
                 }
                 
                 Section {
-                    Picker("From", selection: $fromSelection) {
-                        ForEach(units, id: \.symbol) {
+                    Picker("Conversion", selection: $dimension) {
+                        ForEach(Conversion.allCases, id: \.self) {
+                            Text($0.rawValue.capitalized)
+                        }
+                    }
+                    Picker("From", selection: $inputUnit) {
+                        ForEach(dimension.units, id: \.self) {
                             Text("\($0.symbol)").tag($0)
                         }
                     }
-                    Picker("To", selection: $toSelection) {
-                        ForEach(units, id: \.symbol) {
+                    Picker("To", selection: $outputUnit) {
+                        ForEach(dimension.units, id: \.self) {
                             Text("\($0.symbol)").tag($0)
                         }
                     }
@@ -55,11 +79,15 @@ struct ContentView: View {
                 ToolbarItemGroup(placement: .keyboard) {
                     Spacer()
                     Button("Done") {
-                        valueIsFocused = false
+                        inputIsFocused = false
                     }
                 }
             }
-        }
+            .onChange(of: dimension) { newValue in
+                inputUnit = newValue.units[0]
+                outputUnit = newValue.units[1]
+            }
+        }.navigationViewStyle(.stack)
     }
 }
 
