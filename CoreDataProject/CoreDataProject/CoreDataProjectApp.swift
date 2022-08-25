4aaf9e2958c071160f57d36658d461720c4e9c9e
Alexander Ostrovsky <thealeos@gmail.com>
2022-08-25 22:29:10 +0300

    Create project `CoreDataProject`

---
+++ CoreDataProject/CoreDataProject/CoreDataProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoreDataProjectApp.swift
+//  CoreDataProject
+//
+//  Created by Alexander Ostrovsky on 25.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct CoreDataProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
