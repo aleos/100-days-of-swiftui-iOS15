f17cc42af368b838b347fa123e8d73d0baa39669
Alexander Ostrovsky <thealeos@gmail.com>
2022-08-25 22:38:07 +0300

    Add `DataController` and a core data model.

---
+++ CoreDataProject/CoreDataProject/DataController.swift
@@ -0,0 +1,22 @@
+//
+//  DataController.swift
+//  CoreDataProject
+//
+//  Created by Alexander Ostrovsky on 25.08.2022.
+//
+
+import CoreData
+import Foundation
+
+class DataController: ObservableObject {
+    let container = NSPersistentContainer(name: "CoreDataProject")
+    
+    init() {
+        container.loadPersistentStores { description, error in
+            guard error == nil else {
+                print("Core Data failed to load: \(error!.localizedDescription)")
+                return
+            }
+        }
+    }
+}
