357552c2e23f9db5a3340417e216d438d8a4dcfa
Alexander Ostrovsky <thealeos@gmail.com>
2022-08-27 17:40:33 +0300

    Add `Movie` core data class

---
+++ CoreDataProject/CoreDataProject/Movie/Movie+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Movie+CoreDataClass.swift
+//  CoreDataProject
+//
+//  Created by Alexander Ostrovsky on 27.08.2022.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Movie)
+public class Movie: NSManagedObject {
+
+}
