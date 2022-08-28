ae25391c22433d1fd66a29c65ac86dd19befeaed
Alexander Ostrovsky <thealeos@gmail.com>
2022-08-28 17:05:55 +0300

    Create a new app `Instafilter`

---
+++ Instafilter/Instafilter/InstafilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstafilterApp.swift
+//  Instafilter
+//
+//  Created by Alexander Ostrovsky on 28.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct InstafilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
