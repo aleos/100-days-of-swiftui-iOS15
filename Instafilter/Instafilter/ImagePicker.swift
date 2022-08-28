564f886b5e306346bc124e1ddfe2802665dbf30d
Alexander Ostrovsky <thealeos@gmail.com>
2022-08-28 22:06:24 +0300

    Improve error handling in `ImagePicker`

--- Instafilter/Instafilter/ImagePicker.swift
+++ Instafilter/Instafilter/ImagePicker.swift
@@ -21,13 +21,21 @@ struct ImagePicker: UIViewControllerRepresentable {
         }
         
         func picker(_ picker: PHPickerViewController, didFinishPicking results: [PHPickerResult]) {
-            picker.dismiss(animated: true)
+            guard !results.isEmpty else {
+                picker.dismiss(animated: true)
+                return
+            }
             
             guard let provider = results.first?.itemProvider else { return }
             
             if provider.canLoadObject(ofClass: UIImage.self) {
-                provider.loadObject(ofClass: UIImage.self) { image, _ in
-                    self.parent.image = image as? UIImage
+                picker.dismiss(animated: true)
+                provider.loadObject(ofClass: UIImage.self) { image, error in
+                    if let error {
+                        print("[ImagePicker] Error while loading image: \(error)")
+                    } else {
+                        self.parent.image = image as? UIImage
+                    }
                 }
             }
         }
@@ -36,6 +44,7 @@ struct ImagePicker: UIViewControllerRepresentable {
     func makeUIViewController(context: Context) -> PHPickerViewController {
         var config = PHPickerConfiguration()
         config.filter = .images
+        config.selectionLimit = 1
         
         let picker = PHPickerViewController(configuration: config)
         picker.delegate = context.coordinator
