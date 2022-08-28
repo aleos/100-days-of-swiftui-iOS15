56f7e8a8765e64fca185f48328c6e3f0ee53c76b
Alexander Ostrovsky <thealeos@gmail.com>
2022-08-28 13:18:31 +0300

    Add a CoreData model and its controller

---
+++ Friendface/Friendface/CoreData/DataController.swift
@@ -0,0 +1,24 @@
+//
+//  DataController.swift
+//  CoreDataProject
+//
+//  Created by Alexander Ostrovsky on 25.08.2022.
+//
+
+import CoreData
+import Foundation
+
+class DataController: ObservableObject {
+    let container = NSPersistentContainer(name: "Friendface")
+    
+    init() {
+        container.loadPersistentStores { description, error in
+            guard error == nil else {
+                print("Core Data failed to load: \(error!.localizedDescription)")
+                return
+            }
+            
+            self.container.viewContext.mergePolicy = NSMergePolicy.mergeByPropertyObjectTrump
+        }
+    }
+}
