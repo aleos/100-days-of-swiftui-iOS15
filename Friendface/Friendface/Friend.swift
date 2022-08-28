a108023b8009912de6618aabdf58f6f3dcc933b7
Alexander Ostrovsky <thealeos@gmail.com>
2022-08-28 10:23:00 +0300

    Add `User` and `Friend` structs

---
+++ Friendface/Friendface/Friend.swift
@@ -0,0 +1,13 @@
+//
+//  Friend.swift
+//  Friendface
+//
+//  Created by Alexander Ostrovsky on 28.08.2022.
+//
+
+import Foundation
+
+struct Friend: Codable {
+    var id: String
+    var name: String
+}
---
+++ Friendface/Friendface/User.swift
@@ -0,0 +1,21 @@
+//
+//  User.swift
+//  Friendface
+//
+//  Created by Alexander Ostrovsky on 28.08.2022.
+//
+
+import Foundation
+
+struct User: Codable {
+    var id: String
+    var isActive: Bool
+    var name: String
+    var age: Int
+    var email: String
+    var address: String
+    var about: String
+    var registered: Date
+    var tags: [String]
+    var friends: [Friend]
+}
