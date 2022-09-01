28d53d7c0138c6b4f4ceb3a0a3fc741ffead2850
Alexander Ostrovsky <thealeos@gmail.com>
2022-09-01 19:54:45 +0300

    Add a new project `BucketList`

---
+++ BucketList/BucketList/BucketListApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucketListApp.swift
+//  BucketList
+//
+//  Created by Alexander Ostrovsky on 01.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct BucketListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BucketList/BucketList/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  BucketList
+//
+//  Created by Alexander Ostrovsky on 01.09.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
