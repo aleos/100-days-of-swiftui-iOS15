82c143293994cf04e08df167fa387d9ed0baa96f
Alexander Ostrovsky <thealeos@gmail.com>
2022-09-01 20:34:32 +0300

    Read/write a file in the documents directory; add extension to FileManager, that can decode a file by a given filename

--- BucketList/BucketList/ContentView.swift
+++ BucketList/BucketList/ContentView.swift
@@ -7,29 +7,58 @@
 
 import SwiftUI
 
-struct User: Identifiable, Comparable {
-    let id = UUID()
-    let firstName: String
-    let lastName: String
+struct ContentView: View {
+
+    var body: some View {
+        Text("Hello World")
+            .onTapGesture {
+                let str = "Test Message"
+                let url = getDocumentsDirectory().appendingPathComponent("message.txt")
+                
+                do {
+                    try str.write(to: url, atomically: true, encoding: .utf8)
+                    let input = try String(contentsOf: url)
+                    print(input)
+                } catch {
+                    print(error.localizedDescription)
+                }
+            }
+    }
     
-    static func < (lhs: User, rhs: User) -> Bool {
-        lhs.lastName < rhs.lastName
+    func getDocumentsDirectory() -> URL {
+        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        return paths[0]
     }
 }
 
-struct ContentView: View {
-    
-    let users = [
-            User(firstName: "Arnold", lastName: "Rimmer"),
-            User(firstName: "Kristine", lastName: "Kochanski"),
-            User(firstName: "David", lastName: "Lister"),
-        ].sorted()
-
-        var body: some View {
-            List(users) { user in
-                Text("\(user.lastName), \(user.firstName)")
-            }
+extension FileManager {
+    func decode<T: Decodable>(_ file: String) -> T {
+        let url = getDocumentsDirectory().appendingPathExtension(file)
+        guard FileManager.default.fileExists(atPath: url.absoluteString) else {
+            fatalError("Failed to locate file \(file).")
         }
+
+        guard let data = try? Data(contentsOf: url) else {
+            fatalError("Failed to load \(file) from file.")
+        }
+
+        let decoder = JSONDecoder()
+        
+        let formatter = DateFormatter()
+        formatter.dateFormat = "y-MM-dd"
+        decoder.dateDecodingStrategy = .formatted(formatter)
+
+        guard let loaded = try? decoder.decode(T.self, from: data) else {
+            fatalError("Failed to decode \(file) from file.")
+        }
+
+        return loaded
+    }
+    
+    func getDocumentsDirectory() -> URL {
+        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        return paths[0]
+    }
 }
 
 struct ContentView_Previews: PreviewProvider {
