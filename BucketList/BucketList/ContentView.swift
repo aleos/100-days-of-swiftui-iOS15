48ad5eed4ba0bf7013396298776e2bee232981ac
Alexander Ostrovsky <thealeos@gmail.com>
2022-09-07 22:35:17 +0300

    Increase plus button tap zone

--- BucketList/BucketList/ContentView.swift
+++ BucketList/BucketList/ContentView.swift
@@ -48,12 +48,12 @@ struct ContentView: View {
                             viewModel.addLocation()
                         } label: {
                             Image(systemName: "plus")
+                                .padding()
+                                .background(.black.opacity(0.75))
+                                .foregroundColor(.white)
+                                .font(.title)
+                                .clipShape(Circle())
                         }
-                        .padding()
-                        .background(.black.opacity(0.75))
-                        .foregroundColor(.white)
-                        .font(.title)
-                        .clipShape(Circle())
                         .padding(.trailing)
                     }
                 }
