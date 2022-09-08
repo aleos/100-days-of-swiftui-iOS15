83a5f7fa1954c7704c44c14d66d71659793af40a
Alexander Ostrovsky <thealeos@gmail.com>
2022-09-08 23:39:54 +0300

    Create a new project `AccessibilitySandbox‌`

---
+++ AccessibilitySandbox‌/AccessibilitySandbox‌/AccessibilitySandbox_App.swift
@@ -0,0 +1,17 @@
+//
+//  AccessibilitySandbox_App.swift
+//  AccessibilitySandbox‌
+//
+//  Created by Alexander Ostrovsky on 08.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct AccessibilitySandbox_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
