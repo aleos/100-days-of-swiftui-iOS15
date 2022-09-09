b05861a6df59e3ee15eac6d36f4e390a88d0d8dc
Alexander Ostrovsky <thealeos@gmail.com>
2022-09-09 22:20:02 +0300

    Add accessibility tags

--- AccessibilitySandbox‌/AccessibilitySandbox‌/ContentView.swift
+++ AccessibilitySandbox‌/AccessibilitySandbox‌/ContentView.swift
@@ -10,28 +10,31 @@ import SwiftUI
 struct ContentView: View {
     
     let pictures = [
-            "ales-krivec-15949",
-            "galina-n-189483",
-            "kevin-horstmann-141705",
-            "nicolas-tissot-335096"
-        ]
+        "ales-krivec-15949",
+        "galina-n-189483",
+        "kevin-horstmann-141705",
+        "nicolas-tissot-335096"
+    ]
     
     let labels = [
-            "Tulips",
-            "Frozen tree buds",
-            "Sunflowers",
-            "Fireworks",
-        ]
+        "Tulips",
+        "Frozen tree buds",
+        "Sunflowers",
+        "Fireworks",
+    ]
     
     @State private var selectedPicture = Int.random(in: 0...3)
     
     var body: some View {
         Image(pictures[selectedPicture])
-                    .resizable()
-                    .scaledToFit()
-                    .onTapGesture {
-                        selectedPicture = Int.random(in: 0...3)
-                    }
+            .resizable()
+            .scaledToFit()
+            .onTapGesture {
+                selectedPicture = Int.random(in: 0...3)
+            }
+            .accessibilityLabel(labels[selectedPicture])
+            .accessibilityAddTraits(.isButton)
+            .accessibilityRemoveTraits(.isImage)
     }
 }
 
