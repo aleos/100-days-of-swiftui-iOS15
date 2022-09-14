82417857030a9029fbb865f46bcee994402a9e21
Alexander Ostrovsky <thealeos@gmail.com>
2022-09-14 22:23:31 +0300

    Add accessibility label for crew members names, removing dot
    Dot in the middle of the name makes a long pause. So it sounds better without dots in the middle of the name

--- Moonshot/Moonshot/MissionView.swift
+++ Moonshot/Moonshot/MissionView.swift
@@ -91,6 +91,7 @@ struct MissionView: View {
                                     Text(crewMember.astronaut.name)
                                         .foregroundColor(.white)
                                         .font(.headline)
+                                        .accessibilityLabel(crewMember.astronaut.name.replacingOccurrences(of: ".", with: " "))
                                     Text(crewMember.role)
                                         .foregroundColor(.secondary)
                                 }
