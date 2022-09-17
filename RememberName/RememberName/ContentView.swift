2cfee35a27f6a9901f69009c697cb4e4d433c767
Alexander Ostrovsky <thealeos@gmail.com>
2022-09-17 21:19:50 +0300

    Add image picker

--- RememberName/RememberName/ContentView.swift
+++ RememberName/RememberName/ContentView.swift
@@ -10,6 +10,10 @@ import SwiftUI
 struct ContentView: View {
     
     @StateObject private var friends = Friends()
+    @State private var showingImagePicker = false
+    @State private var showingFriendFillName = false
+    @State private var friendImage: UIImage?
+    @State private var friendName: String = ""
     
     var body: some View {
         NavigationView {
@@ -17,17 +21,38 @@ struct ContentView: View {
                 HStack {
                     Image(uiImage: friend.photo)
                         .resizable()
-                        .scaledToFill()
+                        .aspectRatio(1, contentMode: .fill)
+                        .frame(height: 100)
                         .clipShape(Circle())
                     Text(friend.name)
                         .font(.headline)
                 }
             }
             .navigationTitle("Friends")
+            .onChange(of: friendImage) { _ in showingFriendFillName = true }
+            .sheet(isPresented: $showingImagePicker) {
+                ImagePicker(image: $friendImage)
+            }
+            .sheet(isPresented: $showingFriendFillName) {
+                VStack {
+                    Image(uiImage: friendImage ?? UIImage())
+                        .resizable()
+                        .aspectRatio(1, contentMode: .fill)
+                        .frame(maxHeight: 300)
+                        .clipShape(Circle())
+                        .background(.red)
+                    TextField("Name", text: $friendName)
+                        .font(.headline)
+                        .multilineTextAlignment(.center)
+                        .background(.green)
+                    Spacer()
+                }
+                .background(.blue)
+            }
             .toolbar {
                 ToolbarItem(placement: .navigationBarTrailing) {
                     Button {
-                        
+                        showingImagePicker = true
                     } label: {
                         Image(systemName: "plus")
                     }
