e98ad2510f1715b4270a64ce7e8be386eb90fc49
Alexander Ostrovsky <thealeos@gmail.com>
2022-09-20 23:42:14 +0300

    Clean content

--- HotProspects/HotProspects/ContentView.swift
+++ HotProspects/HotProspects/ContentView.swift
@@ -9,25 +9,8 @@ import SwiftUI
 
 struct ContentView: View {
     
-    @State private var selectedTab = "One"
-
     var body: some View {
-        TabView(selection: $selectedTab) {
-            Text("Tab 1")
-                .onTapGesture {
-                    selectedTab = "Two"
-                }
-                .tabItem {
-                    Label("One", systemImage: "star")
-                }
-                .tag("One")
-
-            Text("Tab 2")
-                .tabItem {
-                    Label("Two", systemImage: "circle")
-                }
-                .tag("Two")
-        }
+        Text("")
     }
 }
 
