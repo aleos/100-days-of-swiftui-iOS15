8d1089973fa519c7a48bc22b8df592d9cda3ab89
Alexander Ostrovsky <thealeos@gmail.com>
2022-09-21 23:08:57 +0300

    Add a custom objectWillChange.send() call

--- HotProspects/HotProspects/ContentView.swift
+++ HotProspects/HotProspects/ContentView.swift
@@ -7,10 +7,28 @@
 
 import SwiftUI
 
+@MainActor class DelayedUpdater: ObservableObject {
+    var value = 0 {
+        willSet {
+            objectWillChange.send()
+        }
+    }
+    
+    init() {
+        for i in 1...10 {
+            DispatchQueue.main.asyncAfter(deadline: .now() + Double(i)) {
+                self.value += 1
+            }
+        }
+    }
+}
+
 struct ContentView: View {
     
+    @StateObject private var updater = DelayedUpdater()
+    
     var body: some View {
-        Text("")
+        Text("Value is \(updater.value)")
     }
 }
 
